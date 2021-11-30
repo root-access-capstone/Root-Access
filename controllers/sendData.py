3e4366039f41961c7c02efb2ad475eacb49ff774
Jon Martin <gamet@gmail.com>
2021-11-30 14:01:08 -0600

    Documentation.

--- controllers/sendData.py
+++ controllers/sendData.py
@@ -1,16 +1,17 @@
-# Proprietary
-from data_to_mysql import send_data
-
 # Third Party
 from datetime import datetime
 
+# Proprietary
+from data_to_mysql import send_data
 
-def checkIfDataNeedsSent(lastMinuteSent, temp, hum, moisture, timeLightOn, timestamp, envId):
-	minutesToSendOn = [0, 15, 30, 45]
-	now = datetime.now()
-	minute = now.minute
-	if minute in minutesToSendOn:
-		if minute != lastMinuteSent:
-			send_data(f'{envId},{timestamp},0,{hum},{moisture},{temp},0')
-			lastMinuteSent = minute
-	return lastMinuteSent
+
+def checkIfDataNeedsSent(lastMinuteSent, temp, hum, moisture, timeLightOn, timestamp, envId) -> None:
+    """If the time is right (every 15 minutes), calls send_data"""
+    minutesToSendOn = [0, 15, 30, 45]
+    now = datetime.now()
+    minute = now.minute
+    if minute in minutesToSendOn:
+        if minute != lastMinuteSent:
+            send_data(f'{envId},{timestamp},0,{hum},{moisture},{temp},0')
+            lastMinuteSent = minute
+    return lastMinuteSent
