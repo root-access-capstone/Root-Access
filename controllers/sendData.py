0c0056925033d0751e1130a81fc483be85b6838e
Jon Martin <gamet@gmail.com>
2021-12-02 15:42:24 -0600

    Restructured and abstracted majority of code. Consolidated data-sending scripts. Better error handling.

--- controllers/sendData.py
+++ controllers/sendData.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 # Proprietary
-from data_to_mysql import send_data
+from controllers.database import Database, SensorData, new_data_object
 
 
 def checkIfDataNeedsSent(lastMinuteSent, temp, hum, moisture, timeLightOn, timestamp, envId) -> None:
@@ -15,3 +15,27 @@ def checkIfDataNeedsSent(lastMinuteSent, temp, hum, moisture, timeLightOn, times
             send_data(f'{envId},{timestamp},{timeLightOn},{hum},{moisture},{temp},{envId}')
             lastMinuteSent = minute
     return lastMinuteSent
+
+def send_data(data:str) -> bool:
+    """Sends data to database.
+    Returns 1 if success, 0 otherwise."""
+    db = Database()
+    data = new_data_object(data)
+    try:
+        db.Session.add(data)
+        db.Session.commit()
+        result = db.Session.query(SensorData).all()
+        db.Session.close()
+        if result:
+            print('Stored sensor data in database.')
+        else:
+            print('** Error: Failed to query database.')
+    except Exception as error:
+        print('**Error adding to or querying database: ', error)
+        return 0
+    return 1
+
+if __name__ == '__main__':
+    incoming = '0,2021-04-22 02:22:22,2,4,100,10,3'
+    result = send_data(incoming)
+    assert result, "Failed to send data."
