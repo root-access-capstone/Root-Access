fed9c1c7fc60c0f3a95da7111cc70df44c23b2d5
Jon Martin <gamet@gmail.com>
2021-12-07 16:29:25 -0600

    Changed to accept cumulative minutes.

--- controllers/powerConsumption.py
+++ controllers/powerConsumption.py
@@ -5,7 +5,7 @@ def wattsToKWH(x:int) -> int:
     """Convert watts to kilowatt hours"""
     return x/3600000
 
-def measurePowerConsumption(pumpStartTime:datetime=None, lampStartTime:datetime=None) -> int:
+def measurePowerConsumption(pumpMinutes:int=0, lampMinutes:int=0) -> int:
     """Measures power consumption in KWH"""
     watt_usage_ref = { # Measured in Watts
         'lamp': 13, # Actual
@@ -14,14 +14,13 @@ def measurePowerConsumption(pumpStartTime:datetime=None, lampStartTime:datetime=
         'arduino': 5 # TBD
     }
 
-    endTime = datetime.now()
     powerConsumption = 0
     try:
         for k, v in watt_usage_ref.items():
-            if k == 'lamp' and lampStartTime:
-                powerConsumption += (v * ((endTime-lampStartTime).seconds))
-            elif k == 'pump' and pumpStartTime:
-                powerConsumption += (v * ((endTime-pumpStartTime).seconds))
+            if k == 'lamp' and lampMinutes:
+                powerConsumption += (v * (lampMinutes*60))
+            elif k == 'pump' and pumpMinutes:
+                powerConsumption += (v * (pumpMinutes*60))
             elif k != 'lamp' and k != 'pump':
                 powerConsumption += (v * (900))
     except Exception as error:
@@ -31,7 +30,6 @@ def measurePowerConsumption(pumpStartTime:datetime=None, lampStartTime:datetime=
     return powerConsumptionKWH
 
 if __name__ == '__main__':
-    time = datetime.now() - timedelta(minutes=7)
-    pc = measurePowerConsumption(lampStartTime=time)
-    assert round(pc, 6) == 0.005266, 'Incorrect power consumption output.'
-    print(pc, 'kwh')
+    pc = measurePowerConsumption(lampMinutes=7)
+    assert round(pc, 6) == 0.005267, 'Incorrect power consumption output: '+str(round(pc, 6))
+    print(round(pc, 6), 'kwh')
