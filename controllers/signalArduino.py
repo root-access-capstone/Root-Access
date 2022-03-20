458852c002964a753dc7eefe6b717087d4ee3b18
Jon Martin <gamet@gmail.com>
2022-03-20 18:04:07 -0400

    Added docs

--- controllers/signalArduino.py
+++ controllers/signalArduino.py
@@ -1,6 +1,8 @@
 import serial
 
-def determineSignalToSend(isPumpOn:bool, isLightOn:bool, board:serial.Serial):
+def determineSignalToSend(isPumpOn:bool, isLightOn:bool, board:serial.Serial) -> None:
+    """Determines the signal to send to the arduino depending on which
+    accessories are on or off"""
     if isPumpOn:
         if isLightOn:
             board.write(b'A')
