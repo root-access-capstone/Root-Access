d227cda2583a75268c778076909863e38aa63f98
Jon Martin <gamet@gmail.com>
2022-03-20 18:00:27 -0400

    Docs so the linter will be quiet

--- controllers/dataArray.py
+++ controllers/dataArray.py
@@ -5,9 +5,11 @@ class DataArray():
     def __init__(self, inflectionPoint:int, length:int) -> None:
         self.data = [inflectionPoint] * length
 
-    def add(self, x:str):
+    def add(self, x:str) -> None:
+        """Adds the new value and removes the oldest"""
         self.data.append(int(x))
         self.data.pop(0)
 
-    def getAvg(self):
+    def getAvg(self) -> float:
+        """Just uses the statistics mean function"""
         return mean(self.data)
