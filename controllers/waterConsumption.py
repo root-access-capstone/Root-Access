1b110e3ad3ec516a9355dafb1f56014e9dd8d349
Jon Martin <gamet@gmail.com>
2022-03-20 17:57:28 -0400

    We believe this makes it go by seconds now Also added docs

--- controllers/waterConsumption.py
+++ controllers/waterConsumption.py
@@ -1,2 +1,4 @@
-def measureWaterConsumption(timePumpOn):
-    return timePumpOn * (100000 / 60)
+def measureWaterConsumption(timePumpOn) -> int:
+    """Takes the time the pump has been on (not sure the unit)
+    times the flowrate & converts to seconds"""
+    return timePumpOn * (100000 / 3600)
