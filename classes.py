e48dbf9cf16bf4c5e85360056cf507503e4f8e51
Jon Martin <gamet@gmail.com>
2022-04-11 11:31:13 -0500

    Cleaned new_calc and handles None for time_turned_off

--- classes.py
+++ classes.py
@@ -105,32 +105,34 @@ class Peripheral:
         # now = datetime.now()
         seconds_diff = None
         # Intervals are every time we store data
-        if not self.is_on and self.time_turned_off >= (now - timedelta(minutes=15)):
-            # Turned off this interval
-            same_interval = (
-                self.time_turned_off.minute // 15== self.time_turned_on.minute // 15)
-            if same_interval:
-                time_difference = self.time_turned_off - self.time_turned_on
-                logging.debug(" %s turned off this interval, storing for roughly %s minutes",
-                    self.name, time_difference)
-                seconds_diff = time_diff_to_interval_seconds(time_difference)
-            else:
-                time_difference = timedelta(minutes=self.time_turned_off.minute % 15)
-                logging.debug(" %s turned off this interval, storing for roughly %s minutes",
-                    self.name, time_difference)
-                seconds_diff = time_diff_to_interval_seconds(time_difference)
-        elif self.is_on:
+        if self.is_on: # need to test w/ time_turned_off set to None
             if self.time_turned_on.minute//15 == now.minute//15:
+                # Turned on this interval, still on
                 time_difference = now - self.time_turned_on
                 logging.debug(" %s turned on this interval, storing for roughly %s minutes",
                     self.name ,time_difference)
-                seconds_diff = time_diff_to_interval_seconds(time_difference)
-            else: # turned on in a different interval, but still on
+                seconds_diff = time_difference.seconds
+            else:
+                # Turned on previous interval, still on
                 time_difference = timedelta(minutes=15)
-                logging.debug(" %s turned on in a previous interval, storing for full interval - %s minutes",
+                logging.debug(" %s turned on in a previous interval, storing for %s minutes",
                     self.name ,time_difference)
-                seconds_diff = time_diff_to_interval_seconds(time_difference)
+                seconds_diff = time_difference.seconds
+        elif self.time_turned_off and self.time_turned_off >= (now - timedelta(minutes=15)):
+            if self.time_turned_off.minute//15 == self.time_turned_on.minute//15:
+                # Turned on & off this interval
+                time_difference = self.time_turned_off - self.time_turned_on
+                logging.debug(" %s turned off this interval, storing for roughly %s minutes",
+                    self.name, time_difference)
+                seconds_diff = time_difference.seconds
+            else:
+                # Turned on in a previous interval, off this interval
+                time_difference = timedelta(minutes=self.time_turned_off.minute % 15)
+                logging.debug(" %s turned off this interval, storing for roughly %s minutes",
+                    self.name, time_difference)
+                seconds_diff = time_difference.seconds
         else:
+            # Off this interval and not on recently
             logging.debug(" %s wasn't on this interval, storing 0 seconds",
                 self.name)
             seconds_diff = 0
