9421d1aac976742ed7eb00828f3905249670c8b6
Jon Martin <gamet@gmail.com>
2022-04-12 15:32:42 -0500

    Logging

--- arduinoDriver.py
+++ arduinoDriver.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 import serial
 import time
+import logging
 
 # Proprietary
 from controllers.sendEmail import notifyLowWater, notifyWaterFilled
@@ -9,8 +10,13 @@ from controllers.sendData import checkIfDataNeedsSent
 from controllers.signalArduino import determineSignalToSend
 from controllers.dataArray import DataArray
 from controllers.database import Database
-
 from classes import FloatSensor, Peripheral
+
+
+logging.basicConfig(level=logging.ERROR,
+    filemode="w",
+    filename="error_log.log",
+)
 
 board = serial.Serial(
     port = '/dev/ttyACM0',
@@ -30,7 +36,6 @@ moistureArray = DataArray(450, 5)
 
 light_fixt = Peripheral(name="Light", critical_value=100)
 pump = Peripheral(name="Pump", critical_value=400)
-
 
 floatFlag = FloatSensor()
 emailSent = False
@@ -92,9 +97,9 @@ while True:
             signalSentBool = False
             print(output)
         else:
-            print("Incomplete board output: ", output)
+            logging.error(" Incomplete board output: %s", output)
             continue
     except Exception as error:
-        print('**Error reading board: ', error)
+        logging.error(" Error reading board: %s", error)
     finally:
         determineSignalToSend(False, False, board)
