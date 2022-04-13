1ad73f755d331570ea809be6beaeaec0731c5843
Jon Martin <gamet@gmail.com>
2022-04-12 20:16:50 -0500

    Added more unit tests and implemented interval_seconds_on

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -74,7 +74,7 @@ def test_calc_time_on_same_interval():
     five_mins_ago = now - timedelta(minutes=5)
     five_mins_seconds = 5 * 60
     lamp = Lamp(is_on=True, time_turned_on=five_mins_ago)
-    assert lamp.calculate_time_on(now) == five_mins_seconds
+    assert lamp.get_interval_seconds_on(now) == five_mins_seconds
 
 def test_calc_time_on_diff_interval():
     """Tests the Lamp's calculate_time_on method
@@ -83,7 +83,7 @@ def test_calc_time_on_diff_interval():
     twenty_mins_ago = now - timedelta(minutes=20)
     interval_seconds = 15 * 60
     lamp = Lamp(is_on=True, time_turned_on=twenty_mins_ago)
-    assert lamp.calculate_time_on(now) == interval_seconds
+    assert lamp.get_interval_seconds_on(now) == interval_seconds
 
 def test_calc_time_off_same_interval():
     """Tests the Lamp's calculate_time_on method
@@ -92,9 +92,9 @@ def test_calc_time_off_same_interval():
     seven_mins_ago = now - timedelta(minutes=7)
     five_mins_ago = now - timedelta(minutes=5)
     two_mins_seconds = 2 * 60
-    lamp = Lamp(is_on=False, time_turned_on=seven_mins_ago,
-        time_turned_off=five_mins_ago)
-    assert lamp.calculate_time_on(now) == two_mins_seconds
+    lamp = Lamp(is_on=True, time_turned_on=seven_mins_ago)
+    lamp.set_off(five_mins_ago)
+    assert lamp.get_interval_seconds_on(now) == two_mins_seconds
 
 def test_calc_time_off_diff_interval():
     """Tests the Lamp's calculate_time_on method
@@ -103,14 +103,78 @@ def test_calc_time_off_diff_interval():
     twenty_mins_ago = now - timedelta(minutes=20)
     five_mins_ago = now - timedelta(minutes=5)
     nine_mins_seconds = 9 * 60
-    lamp = Lamp(is_on=False, time_turned_on=twenty_mins_ago,
-        time_turned_off=five_mins_ago)
-    assert lamp.calculate_time_on(now) == nine_mins_seconds
+    lamp = Lamp(is_on=True, time_turned_on=twenty_mins_ago)
+    lamp.set_off(five_mins_ago)
+    assert lamp.get_interval_seconds_on(now) == nine_mins_seconds
 
 def test_calc_time_twice_same_interval():
     """Tests the Lamp's calculate_time_on method
     when turned on & off twice in the same interval"""
-    pass
+    now = datetime(2022,4,11,9,21,56)
+    two_mins_later = now + timedelta(minutes=2)
+    four_mins_later = now + timedelta(minutes=4)
+    five_mins_ago = now - timedelta(minutes=5)
+    five_mins_seconds = 5 * 60 # Difference of first round
+    seven_mins_seconds = 7 * 60 # Difference of first & second
+    lamp = Lamp(is_on=True, time_turned_on=five_mins_ago)
+    lamp.set_off(now)
+    # Have to access the property, assumed same interval
+    assert lamp.interval_seconds_on == five_mins_seconds
+    logging.debug(" Lamp after first round: \n%s", lamp)
+    lamp.set_on(two_mins_later)
+    assert lamp.get_interval_seconds_on(four_mins_later) == seven_mins_seconds
+    logging.debug(" Lamp after second round: \n%s", lamp)
+
+def test_calc_time_thrice_same_interval():
+    """Tests the Lamp's calculate_time_on method
+    when turned on & off thrice in the same interval"""
+    now = datetime(2022,4,11,9,21,56)
+    one_mins_later = now + timedelta(minutes=1)
+    two_mins_later = now + timedelta(minutes=2)
+    two_half_mins_later = now + timedelta(minutes=2, seconds=30)
+    three_mins_later = now + timedelta(minutes=3)
+    three_mins_ago = now - timedelta(minutes=3)
+    five_mins_ago = now - timedelta(minutes=5)
+    two_mins_seconds = 2 * 60 # Difference of first round
+    three_mins_seconds = 3 * 60 # Difference of first & second
+    three_half_mins_seconds = 3 * 60 + 30 # Difference of first & second & third
+    lamp = Lamp(is_on=True, time_turned_on=five_mins_ago)
+    lamp.set_off(three_mins_ago)
+    # Have to access the property, assumed same interval
+    assert lamp.interval_seconds_on == two_mins_seconds
+    logging.debug(" Lamp after first round: \n%s", lamp)
+    lamp.set_on(one_mins_later)
+    lamp.set_off(two_mins_later)
+    # Have to access the property, assumed same interval
+    assert lamp.interval_seconds_on == three_mins_seconds
+    logging.debug(" Lamp after second round: \n%s", lamp)
+    lamp.set_on(two_mins_later)
+    lamp.set_off(two_half_mins_later)
+    assert lamp.get_interval_seconds_on(three_mins_later) == three_half_mins_seconds
+    logging.debug(" Lamp after third round: \n%s", lamp)
+
+def test_calc_time_off_across_two_intervals():
+    """Tests the the Lamp's calculate_time_on method
+    when turned on last interval"""
+    now = datetime(2022,4,11,9,21,56)
+    interval_time = now + timedelta(minutes=8)
+    twenty_mins_later = now + timedelta(minutes=20)
+    next_interval_time = now + timedelta(minutes=23)
+    eight_mins_seconds = 8 * 60
+    eleven_mins_seconds = 11 * 60
+    lamp = Lamp(is_on=True, time_turned_on=now)
+    assert lamp.get_interval_seconds_on(interval_time) == eight_mins_seconds
+    logging.debug(" Lamp after first round: \n%s", lamp)
+    lamp.set_off(twenty_mins_later)
+    assert lamp.get_interval_seconds_on(next_interval_time) == eleven_mins_seconds
+    logging.debug(" Lamp after second round: \n%s", lamp)
+
+def test_calc_time_off_never_on():
+    """Tests the the Lamp's calculate_time_on method
+    when not yet turned on"""
+    now = datetime(2022,4,11,9,29,56)
+    lamp = Lamp(is_on=False)
+    assert lamp.get_interval_seconds_on(now) == 0
 
 def test_data_class_init():
     """Tests the Data class's DataArray post_init
