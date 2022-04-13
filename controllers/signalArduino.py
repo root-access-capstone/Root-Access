7935a3969b5cf3ec257ce0399cc5063b549f4825
Jon Martin <gamet@gmail.com>
2022-04-13 15:14:32 -0500

    Logging

--- controllers/signalArduino.py
+++ controllers/signalArduino.py
@@ -1,4 +1,5 @@
 from serial import Serial
+import logging
 
 def determineSignalToSend(isPumpOn:bool, isLightOn:bool, board:Serial) -> None:
     """Determines the signal to send to the arduino depending on which
@@ -6,10 +7,14 @@ def determineSignalToSend(isPumpOn:bool, isLightOn:bool, board:Serial) -> None:
     if isPumpOn:
         if isLightOn:
             board.write(b'A')
+            logging.debug(" Wrote (both on) to the Arduino")
         else:
             board.write(b'C')
+            logging.debug(" Wrote (pump on) to the Arduino")
     else:
         if isLightOn:
             board.write(b'D')
+            logging.debug(" Wrote (light on) to the Arduino")
         else:
             board.write(b'B')
+            logging.debug(" Wrote (both off) to the Arduino")
